750fbdf56b67336b78d38afa0f150a35add96811
soficia <let942@gmail.com>
2022-07-30 21:47:02 -0300

    fix: exibe relatório de faturamento da empresa

--- desafio6.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-MIN(plan.price) AS 'faturamento_minimo',
-MAX(plan.price) AS 'faturamento_maximo',
-ROUND(AVG(plan.price),2) AS 'faturamento_medio',  
-SUM(plan.price) AS 'faturamento_total'
-FROM
-SpotifyClone.plan AS plan
-INNER JOIN SpotifyClone.users AS users
-ON plan.id = users.user_plan;
